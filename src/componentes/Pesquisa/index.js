d015f451c987c2a3070286cf30a08b581075dead
Dericki <101526290+Dericki@users.noreply.github.com>
2025-04-28 14:30:53 -0300

    criaçao da index dos componentes "Pesquisa" e "Input"

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,12 @@
+function Pesquisa () {
+    return (
+        <>
+        <h2>Já sabe por onde começar?</h2>
+        <h3>Encontre seu livro em nossa estante.</h3>
+        <input placeholder="Escreva sua próxima leitura"
+        />
+        </>
+    )
+}
+
+export default Pesquisa
