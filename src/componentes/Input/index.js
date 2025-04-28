314010597bb998c31f08876361e1b2fd57cf9de8
Dericki <101526290+Dericki@users.noreply.github.com>
2025-04-28 14:34:22 -0300

    criaçao do estilo do input

--- src/componentes/Input/index.js
+++ src/componentes/Input/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Input = input`
+const Input = styled.input`
         border: 1px solid #FFF;
         background: transparent;
         border: 1px solid #FFF;
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,9 +1,12 @@
+import Input from "../Input"
+
 function Pesquisa () {
     return (
         <>
         <h2>Já sabe por onde começar?</h2>
         <h3>Encontre seu livro em nossa estante.</h3>
-        <input placeholder="Escreva sua próxima leitura"
+        <Input 
+            placeholder="Escreva sua próxima leitura"
         />
         </>
     )
