7a13f4f672b1609a1024d15db0e28280e97ecc58
Dericki <101526290+Dericki@users.noreply.github.com>
2025-04-29 10:49:57 -0300

    importando dados da aba "dadosPesquisados"

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input"
 import styled from "styled-components"
 import { useState } from "react"
+import { livros } from "./dadosPesquisa"
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -10,22 +11,19 @@ const PesquisaContainer = styled.section`
         height: 270px;
         width: 100%;
 `
-
 const Titulo = styled.h2`
         color: #FFF;
         font-size: 36px;
         text-align: center;
         width: 100%;
 `
-
 const Subtitulo = styled.h3`
         font-size: 16px;
         font-weight: 500;
         margin-bottom: 40px;
 `
-
 function Pesquisa () {
-    const [textoDigitado, setTextoDigitado] = useState("")
+    const [livrosPesquisado, setlivrosPesquisado] = useState([])
     return (
         <PesquisaContainer>
         <Titulo>Já sabe por onde começar?</Titulo>
