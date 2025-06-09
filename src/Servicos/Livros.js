8196d2e9198c427122b30a6a7a0112dac437ab96
Dericki <101526290+Dericki@users.noreply.github.com>
2025-06-09 10:36:33 -0300

    Criando get no arquivo "Livros"

--- src/Servicos/Livros.js
+++ src/Servicos/Livros.js
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+const LivrosAPI = axios.create({baseURL: "http://localhost:8000/livros"})
+
+function getLivros(){
+    const response= LivrosAPI.get("/")
+
+    return response.data
+}
+
